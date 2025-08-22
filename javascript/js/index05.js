7ea043a24b7d377c5b05cbbb96ca30e963c60fa2
Kamran Manzoor <kamranmanzoor1448@gmail.com>
2025-08-23 00:46:51 +0500

    js modify

---
+++ javascript/js/index05.js
@@ -0,0 +1,136 @@
+// string
+
+// let username = "   JohnDoe  ";
+// let str = '   kal ho na ho   ';
+// let text = `   Phir saheee   `;
+
+// console.log(username, typeof username);
+// console.log(str, typeof str);
+// console.log(text, typeof text);
+
+// // length
+
+// console.log("length of username:", username.length);
+// console.log("length of str:", str.length);
+// console.log("length of text:", text.length);
+
+
+// // slice
+// console.log("slice of username:", username.slice(0, 4));
+// console.log("slice of str:", str.slice(4, 7));
+// console.log("slice of text:", text.slice(0, 4));
+
+// // trim
+// console.log("trimmed str:", str.trim());
+// console.log("trimmed username:", username.trim());
+// let spaceRemove = username.trim();
+// console.log(spaceRemove + " Length:- "  + spaceRemove.length);
+// console.log("trimmed text:", text.trim());
+
+// // trimStart
+// console.log("trimmed start of username:", username.trimStart());
+// console.log("trimmed start of str:", str.trimStart());
+// console.log("trimmed start of text:", text.trimStart());
+
+// // trimEnd
+// console.log("trimmed end of username:", username.trimEnd());
+// console.log("trimmed end of str:", str.trimEnd());
+// console.log("trimmed end of text:", text.trimEnd());
+
+
+// // replace
+// console.log("replaced username:", username.replace("John", "Jane"));
+// console.log("replaced str:", str.replace("kal", "aaj"));
+// console.log("replaced text:", text.replace("Phir", "Ab"));
+
+// // replaceAll
+// console.log("replacedAll username:", username.replaceAll("o", "a"));
+// console.log("replacedAll str:", str.replaceAll("a", "o"));
+// console.log("replacedAll text:", text.replaceAll("i", "u"));
+
+// // concat
+// console.log("concat username and str:", username.concat(str));
+// console.log("concat str and text:", str.concat(text));
+// console.log("concat username and text:", username.concat(text));
+
+// // charAt
+// console.log("charAt username:", username.charAt(6));
+// console.log("charAt str:", str.charAt(7));
+// console.log("charAt text:", text.charAt(4));
+
+// // toUpperCase
+// console.log("toUpperCase username:", username.toUpperCase());
+// console.log("toUpperCase str:", str.toUpperCase());
+// console.log("toUpperCase text:", text.toUpperCase());
+
+// // toLowerCase
+// console.log("toLowerCase username:", username.toLowerCase());
+// console.log("toLowerCase str:", str.toLowerCase());
+// console.log("toLowerCase text:", text.toLowerCase());
+
+// // includes
+// console.log("includes username:", username.includes("Johjjh"));
+// console.log("includes str:", str.includes("kal"));
+// console.log("includes text:", text.includes("Phir"));
+
+// split
+// let str1 = "Hello World! i am learning JavaScript";
+// let splitArray = str1.split("  ");
+// console.log("splitArray:", splitArray);
+
+
+// Array
+
+// const myArray = [1, 2, 3, 4, 5];
+// console.log("myArray:", myArray);
+
+// const myArray2 = new Array("sunday","monday","friday");
+// console.log("myArray2:", myArray2);
+
+// // index
+// console.log("index of 3 in myArray:", myArray.indexOf(3));
+// console.log("index of 'monday' in myArray2:", myArray2.indexOf("monday"));
+// console.log(myArray2[2]);
+
+
+// const myArr = [1,2,3,4,"monday", "apple", "orange", true];
+
+// // length
+// console.log("length of myArr:", myArr.length);
+
+
+// // pop
+// console.log("popped element from myArr:", myArr.pop());
+// console.log("myArr after pop:", myArr);
+
+// // push
+// myArr.push("new my");
+// console.log("myArr after push:", myArr);
+
+// // shift
+// console.log("shifted element from myArr:", myArr.shift());
+// console.log("myArr after shift:", myArr);
+
+// // unshift
+// myArr.unshift("new first");
+// console.log("myArr after unshift:", myArr);
+
+// // concat
+// const newArray = myArr.concat([6, 7, 8]);
+// console.log("newArray after concat:", newArray);
+
+// // slice
+// const slicedArray = myArr.slice(2, 5);
+// console.log("slicedArray:", slicedArray);
+
+// // splice
+// const splicedArray = myArr.splice(2, 2, "newElement");
+// console.log("splicedArray:", splicedArray);
+// console.log("myArr after splice:", myArr);
+
+// // toString
+// console.log("myArr toString:", myArr.toString());
+
+// // sort
+// const sortedArray = myArr.sort();
+// console.log("sortedArray:", sortedArray);
---
+++ javascript/js/index06.js
@@ -0,0 +1,126 @@
+// // number
+// let num1 = 10;
+// let num2 = 20.5;
+// let num3 = "1233";
+// let num4 = "3244.44";
+
+// // parseInt
+// console.log("parseInt of num3:", parseInt(num3));
+// console.log("parseInt of num4:", parseInt(num4));
+
+// // parseFloat
+// console.log("parseFloat of num3:", parseFloat(num3));
+// console.log("parseFloat of num4:", parseFloat(num4));
+
+// // toFixed
+// console.log("toFixed of num2:", num2.toFixed(1));
+// console.log("toFixed of num4:", parseFloat(num4).toFixed(1));
+
+// // toString
+// console.log("toString of num1:", num1.toString());
+// console.log("toString of num2:", num2.toString());
+
+// // isNaN
+// console.log("isNaN of num1:", isNaN(num1));
+// console.log("isNaN of num2:", isNaN(num2));
+// console.log("isNaN of num3:", isNaN(num3));
+// console.log("isNaN of num4:", isNaN(num4));
+
+
+// Functions
+
+// types of function:
+/*1. Function Declaration
+2. Function Expression
+3. Arrow Function
+4. IIFE (Immediately Invoked Function Expression)*/
+
+
+// // Declaration
+// function test() {
+//     let text = "Hello , class kasy hu"
+//     console.log(text);
+// }
+
+// test();
+
+
+// // Expression
+// const test2 = function() {
+//     let text = "Hello , class kasy hu"
+//     console.log(text);
+// }
+
+// test2();
+
+// // Arrow
+// const test3 = () => {
+//     let text = "Hello , class kasy hu"
+//     console.log(text);
+// }
+
+// test3();
+
+// // IIFE
+// (function() {
+//     let text = "Hello , class kasy hu, me tu thk ni hu aur aap kaise ho"
+//     console.log(text);
+// })();
+
+
+// // parameters and arguments
+// function greet(name) {
+//     let text = "Hello, " + name;
+//     console.log(text);
+// }
+
+// greet("Alice, kasy hu ");
+
+
+// // return statement
+// function add(a, b) {
+//     return a + b;
+// }
+
+// let sum = add(5, 10);
+// console.log("Sum:", sum);
+
+
+
+// Scope
+
+/*
+Scope
+1. Global Scope
+2. Function Scope = local scope
+3. Block Scope
+4. Lexical Scope
+*/
+
+
+// // global scope
+// let globalVar = "I am a global variable";
+
+// function checkScope() {
+//     let localVar = "I am a local variable";
+//     console.log(globalVar);
+//     console.log(localVar);
+// }
+
+// checkScope();
+
+// // Function Scope
+// function anotherFunction() {
+//     let anotherLocalVar = "I am another local variable";
+//     console.log(globalVar);
+//     console.log(anotherLocalVar);
+// }
+
+// anotherFunction();
+
+
+// // Block Scope
+// {
+//     let blockScopedVar = "I am a block scoped variable";
+//     console.log(blockScopedVar);
+// }
